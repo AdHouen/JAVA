ec0c0cb16df2884ea4b04767f7d5a0b3dfda6584
Adrien <103360104+AdHouen@users.noreply.github.com>
2022-10-07 18:18:57 +0200

    changement

--- 02_projet/Item5.java
+++ 02_projet/Item5.java
@@ -1,11 +1,5 @@
 public class Item5 {
-    // Utiliser la classe java.lang.Math pour afficher des calculs scientifiques 
-    // Lecon retenu : 
-    //     - Chaque methode est indépendante
-    //     - pas besoin de faire la methode addition
-    //     -mettre des parentheses pour mettre les priorités sur les calculs
-    //     - un cacul (add, sous, etc) est toujours en int si l'on veut changer il faut le forcer ((byte) ou (float) etc)
-    //     - System.out.format("%.1f" pour choisir le nombre de décimale
+    
 
     public static void main(String[] args) {
         double i = 4;
@@ -15,6 +9,14 @@ public class Item5 {
         // System.out.format("%.10f", + (Math.cos(c))); //<=== avoir le cosinus avec par exmple 10décimales
         
     }
+
+    // Utiliser la classe java.lang.Math pour afficher des calculs scientifiques 
+    // Lecon retenu : 
+    //     - Chaque methode est indépendante
+    //     - pas besoin de faire la methode addition
+    //     -mettre des parentheses pour mettre les priorités sur les calculs
+    //     - un cacul (add, sous, etc) est toujours en int si l'on veut changer il faut le forcer ((byte) ou (float) etc)
+    //     - System.out.format("%.1f" pour choisir le nombre de décimale
     
     
 }
