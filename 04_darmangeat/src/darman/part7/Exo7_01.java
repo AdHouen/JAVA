786727588b999e3b77230a2d4250fe0b67a38c2c
Adrien <103360104+AdHouen@users.noreply.github.com>
2022-10-18 22:04:45 +0200

    exo 7

--- 04_darmangeat/src/darman/part7/Exo7_01.java
+++ 04_darmangeat/src/darman/part7/Exo7_01.java
@@ -1,9 +1,49 @@
 package darman.part7;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exo7_01 {
 
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Entrer le nombre de valeurs : ");
+		int nombre = scanner.nextInt();
+		int[] monTab = new int[nombre];
+
+		for (int i = 0; i < monTab.length ; i++) {
+			System.out.println("Entrez le nombre numero " + (i + 1) + " :");
+
+			monTab[i] = scanner.nextInt();
+
+		}
+		
+		System.out.println("Les valeurs du tableau sont : " + Arrays.toString(monTab));
+
+		
+		int pointDedepart = monTab[0];
+		boolean drapeau = true;
+		
+		for (int i = 0; i < monTab.length ; i++) {
+			if (pointDedepart != ((monTab[i]-1)+1)) {
+				drapeau = false;
+				break;
+			}
+			pointDedepart++;
+			
+
+
+		}
+		if (drapeau) {
+			
+			System.out.println("Les nombre sont consecutifs");
+
+		} else {
+			
+			System.out.println("Les nombres ne sont pas consecutifs");
+		}
 
 	}
 
--- 04_darmangeat/src/darman/part7/Exo7_02.java
+++ 04_darmangeat/src/darman/part7/Exo7_02.java
@@ -1,9 +1,38 @@
 package darman.part7;
 
-public class Exo7_02 {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
+public class Exo7_02 {
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Combien de valeur voulez vous rentrer ?");
+		int nombre = scanner.nextInt();
+		Integer [] monTab = new Integer[nombre];
+		
+		for (int i = 0; i < monTab.length; i++) {
+			System.out.printf("Veuillez entrer la valeur %s : ", (i+1));
+			monTab [i] = scanner.nextInt();
+			
+		}
+		
+		System.out.println("Les valeurs de votre tableau sont : " + Arrays.toString(monTab));
+		
+		Arrays.sort(monTab, Comparator.reverseOrder());
+		System.out.println("");
+		System.out.println("Les valeurs de votre tableau TRIER DECROISSANT  sont : " + Arrays.toString(monTab));
+		
+		Arrays.sort(monTab);
+		System.out.println("");
+		System.out.println("Les valeurs de votre tableau TRIER CROISSSANT  sont : " + Arrays.toString(monTab));
+	
+		
+		
 
 	}
 
