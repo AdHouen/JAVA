46bd4857bad73e68b2a0db8ce8e05c55ee19dc1a
Adrien <103360104+AdHouen@users.noreply.github.com>
2022-11-01 16:18:57 +0100

    Create UtilitaireBookStore.java

---
+++ TP_FrameWorkAfpa/src/fr/afpa/tool/UtilitaireBookStore.java
@@ -0,0 +1,50 @@
+package fr.afpa.tool;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+
+
+
+
+
+public class UtilitaireBookStore {
+    
+   
+    
+    public static void main(String[] args) {
+        
+        LocalDateTime maintenant;
+        maintenant = LocalDateTime.now();
+        
+      
+        
+        
+        System.out.println(maintenant);
+        
+        
+       
+        
+                
+        
+    }
+   
+    
+
+    static boolean isPretEnRetard (LocalDate dateEmpruntEffective, LocalDate maintenant, int dureeMaxPret) {
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        return false;
+        
+    }
+
+
+}
