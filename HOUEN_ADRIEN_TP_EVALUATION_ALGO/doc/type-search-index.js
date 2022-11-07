47da1c6c5a4f9dd6cefa89d99b1a3d625b94f3de
Adrien <adhouen@yahoo.com>
2022-11-07 13:48:36 +0100

    rectification

--- HOUEN_ADRIEN_TP_EVALUATION_ALGO/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"eval.question","l":"Quest2_Multiple"}];updateSearchResults();
--- TP25_propertiesVM/src/Item1_Recuperation_Proprietes_JVM.java
+++
@@ -1,8 +0,0 @@
-
-public class Item1_Recuperation_Proprietes_JVM {
-	
-	public static void main(String[] args) {
-		
-	}
-
-}
